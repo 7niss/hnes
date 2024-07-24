4828f06828ac7aa1acd6d84c195abb869449e5a8
7niss <159466245+7niss@users.noreply.github.com>
2024-07-24 17:47:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById('closeButton').addEventListener('click', function() {
+    var buttonImage = document.getElementById('buttonImage');
+
+    
+    buttonImage.src = 'button pressed.png';
+
+    
+    setTimeout(function() {
+        
+        buttonImage.src = 'button.png';
+
+        
+        setTimeout(function() {
+            
+            window.close();
+        }, 50);
+    }, 150);
+});
