9130eb8b14ad1a17b9ed0d1bf9896cc25a283c16
7niss <159466245+7niss@users.noreply.github.com>
2024-07-24 17:39:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
 document.getElementById('closeButton').addEventListener('click', function() {
     var buttonImage = document.getElementById('buttonImage');
-
-    // Change the image source to 'button pressed.png'
     buttonImage.src = 'button pressed.png';
-
-    // Wait for 150 milliseconds
-    setTimeout(function() {
-        // Change the image source back to 'button.png'
+    setTimeout(function() 
         buttonImage.src = 'button.png';
-
-        // Wait for a short moment before closing the tab to ensure the image is reverted
         setTimeout(function() {
-            // Close the tab
+
             window.close();
         }, 50);
     }, 150);
