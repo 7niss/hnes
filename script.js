d8680424672c5b001ae669db9c3624e46cc3aaa7
7niss <159466245+7niss@users.noreply.github.com>
2024-08-30 09:06:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ document.addEventListener('DOMContentLoaded', () => {
                         response = `GitHub: <a href="https://github.com/7niss" target="_blank">7niss</a><br>YouTube: <a href="https://www.youtube.com/@styIing" target="_blank">@styIing</a><br>X: <a href="https://x.com/xn1ss" target="_blank">xn1ss</a>`;
                         break;
                     case 'botic':
-                        response = `<a href="https://www.google.com/maps/place/Botic+Lovers/@46.2953131,7.8790369,15z/data=!4m2!3m1!1s0x0:0xc72b9850afbf3159?sa=X&ved=1t:2428&ictx=111" target="_blank">@styIing</a>`;
+                        response = `<a href="https://www.google.com/maps/place/Botic+Lovers/@46.2953131,7.8790369,15z/data=!4m2!3m1!1s0x0:0xc72b9850afbf3159?sa=X&ved=1t:2428&ictx=111" target="_blank">Botic Lovers</a>`;
                         break;
                     default:
                         response = `Command not recognized: ${command}`;
