06e1efe5be079a8eac30b148e5368e57bc2ed581
7niss <159466245+7niss@users.noreply.github.com>
2024-11-20 11:37:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -49,10 +49,6 @@ document.addEventListener('DOMContentLoaded', () => {
                 responseParagraph.innerHTML = response; 
                 output.appendChild(responseParagraph);
 
-                if (![...commandListContent.children].some(item => item.textContent === command)) {
-                    addCommandToList(command);
-                }
-
                 commandInput.value = '';
                 output.scrollTop = output.scrollHeight;  
             }
